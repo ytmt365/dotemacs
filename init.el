46e949d191bb4df2a5c2c77d9201ff4f238f93bb
yutaro matsui <yumatsui.1994@gmail.com>
2017-03-23 13:02:09 +0900

    add undo-tree

--- init.el
+++ init.el
@@ -19,7 +19,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (yatex php-mode matlab-mode markdown-mode google-translate coffee-mode))))
+    (undo-tree yatex php-mode matlab-mode markdown-mode google-translate coffee-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
---
+++ inits/01_undo-tree.el
@@ -0,0 +1,15 @@
+;; undotree.el --- UndoTree
+;;; Commentary:
+;;; Code:
+;; undo-hist ファイルを閉じても再度undo-treeを使える
+(require 'undohist nil t)
+
+;; undo-tree
+(global-undo-tree-mode t)
+(global-set-key (kbd "C-x u") 'undo-tree-visualize)
+(global-set-key (kbd "C-M-z") 'undo-tree-redo)
+
+;; mode name setting
+(setcar (cdr (assq 'undo-tree-mode minor-mode-alist)) " UT")
+
+;;; init-undo-tree.el ends here
